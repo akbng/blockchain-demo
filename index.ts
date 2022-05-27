6641536152a0e8fffb7f4f7b4f8a451dc1bfef35
ankanbag101 <bagankan@gmail.com>
2022-05-27 22:13:16 +0530

    added colors in the terminal output

--- index.ts
+++ index.ts
@@ -1,19 +1,20 @@
 const readlineSync = require("readline-sync");
+import color from "./lib/colors";
 import Block from "./src/Block";
 import Blockchain from "./src/Blockchain";
 
-const usage = `
- _____________________________________________________________________________________________________________
-|                                                                                                             |
-|  Usage:-                                                                                                    |
-|  -> add sender_name receiver_name transfer_amount  # adds a block to blockchain with the given data         |
-|  -> show                                           # shows the list of blocks available in the blockchain   |
-|  -> tamper block_number                            # tampers with the block giver by the number             |
-|  -> check                                          # validates the blockchain                               |
-|  -> help                                           # show this message                                      |
-|  -> exit                                           # close the program                                      |
-|_____________________________________________________________________________________________________________|
-`;
+const usage = `${color.fg.green}
+ ____________________________________________________________________________________________________________
+|                                                                                                            |
+|  ${color.blink}Usage:-${color.reset}${color.fg.green}                                                                                                   |
+|  > add sender_name receiver_name transfer_amount  # adds a block to blockchain with the given data         |
+|  > show                                           # shows the list of blocks available in the blockchain   |
+|  > tamper block_number                            # tampers with the block giver by the number             |
+|  > check                                          # validates the blockchain                               |
+|  > help                                           # show this message                                      |
+|  > exit                                           # close the program                                      |
+|____________________________________________________________________________________________________________|
+${color.reset}`;
 
 let input: string;
 console.log(usage);
@@ -24,17 +25,21 @@ const format = (chain: Array<Block>, limit: number) => {
   const newChain = chain.slice(isLongList ? chain.length - limit : 0);
   newChain.forEach((block, index) => {
     if (index == 0 && !isLongList) return;
-    string += `\n[${index + (isLongList ? 1 : 0)}]\tSender: ${
-      block.data?.from
-    }\tReceiver: ${block.data?.to}\tAmount-Transfered: ${block.data?.amount}`;
+    string += `\n${color.fg.yellow}[${index + (isLongList ? 1 : 0)}]${
+      color.reset
+    }\t${color.fg.magenta}Sender:${color.reset} ${block.data?.from}\t${
+      color.fg.magenta
+    }Receiver:${color.reset} ${block.data?.to}\t${color.fg.magenta}Amount:${
+      color.reset
+    } ${block.data?.amount}`;
   });
-  return string;
+  return string || `${color.fg.yellow}Nothing to show :(${color.reset}`;
 };
 
 const blockchain = Blockchain.create(2);
 
 do {
-  input = readlineSync.question("-> ");
+  input = readlineSync.question(`${color.dim}> ${color.reset}`);
   const commands = input.split(" ");
   const command = commands[0];
   const length = commands.length;
@@ -42,28 +47,28 @@ do {
   switch (command) {
     case "add":
       if (length !== 4) {
-        console.log("<- WRONG NUMBER OF ARGUMENTS!");
+        console.log(color.fg.red, `WRONG NUMBER OF ARGUMENTS!`, color.reset);
         continue;
       }
       const amount = parseFloat(commands[3]);
       if (isNaN(amount)) {
-        console.log("<- Please give a correct Amount");
+        console.log(color.fg.red, `Please give a correct Amount`, color.reset);
         continue;
       }
       blockchain.addBlock(commands[1], commands[2], amount);
       break;
     case "show":
-      process.stdout.write("<- ");
+      process.stdout.write(color.bright + "TXs: " + color.reset);
       console.log(format(blockchain.chain, outputLimit));
       break;
     case "tamper":
       if (length !== 2) {
-        console.log("<- WRONG NUMBER OF ARGUMENTS :(");
+        console.log(color.fg.red, "WRONG NUMBER OF ARGUMENTS :(", color.reset);
         continue;
       }
       const index = parseInt(commands[1]);
       if (index < 1 || index > outputLimit) {
-        console.log("<- WRONG INDEX FOR BLOCK");
+        console.log(color.fg.red, "WRONG INDEX FOR BLOCK", color.reset);
         continue;
       }
       const chainLength = blockchain.chain.length;
@@ -74,17 +79,18 @@ do {
     case "check":
       console.log(
         blockchain.isValid()
-          ? "<- The Blockchain is valid :)"
-          : "<- [CRITICAL] The Blockchain is compromised :("
+          ? `${color.fg.yellow}The Blockchain is valid :)`
+          : `${color.fg.red}[CRITICAL] The Blockchain is compromised :(`,
+        color.reset
       );
       break;
     case "help":
       console.log(usage);
       break;
     case "exit":
-      console.log("<- GOODBYE! :(");
+      console.log(color.fg.cyan, "GOODBYE! :(", color.reset);
       break;
     default:
-      console.log("<- WRONG COMMAND :(");
+      console.log(color.fg.magenta, "WRONG COMMAND :(", color.reset);
   }
 } while (input !== "exit");
